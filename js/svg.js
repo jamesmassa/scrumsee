6688d5aa8661e50e83921559c94ed74a54f0784d
James Massa <james.l.massa@gmail.com>
2019-12-29 22:18:46 -0500

    progressed svg diagram

--- js/svg.js
+++ js/svg.js
@@ -2,18 +2,25 @@ class Svg {
     constructor(container, width, height, margin) {
         this.margin = margin;
 
-        if (width == -1) {
-            width = $(container).width();
-        }
+        // if (width == -1) {
+        //     width = $(container).width();
+        // }
 
-        this.width = width - this.margin.left - this.margin.right;
-        this.height = height - this.margin.top - this.margin.bottom;
+        width = width - this.margin.left - this.margin.right;
+        height = height - this.margin.top - this.margin.bottom;
         this.container = container;
+        this.containerElem = d3.select(container);
 
-        this.svg = d3.select(this.container)
-            .append("svg")
-            .attr("width", this.width)
-            .attr("height", this.height)
+        this.svg = d3.select(container)
+            .append("svg");
+
+        console.log("svg typeof:" + typeof(this.svg));
+
+        this.svg
+            .attr("width", width)
+            .attr("height", height)
+            .attr('viewBox', `0 0 ${width} ${height}`)
+            .attr('preserveAspectRatio', 'xMinYMid')
             .append("g")
             .attr("class", "main-group")
             .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
@@ -24,16 +31,25 @@ class Svg {
     get svg(){return this._svg;}
     set svg(svg){this._svg = svg;}
 
-    get width(){return this._width;}
-    set width(width){this._width = width;}
+    get width(){return this.svg.attr("width"); }
 
-    get height(){return this._height;}
-    set height(height){this._height = height;}
+    set width(width){
+        this.svg.attr("width", width);
+        this.height = Math.round(width / this.aspect);
+    }
+
+    get height(){return this.svg.attr("height"); }
+    set height(height){this.svg.attr("height", height); }
 
     get margin(){return this._margin;}
     set margin(margin){this._margin = margin;}
 
     get container(){return this._container;}
     set container(container){this._container = container;}
+
+    get containerElem(){return this._containerElem;}
+    set containerElem(containerElem){this._containerElem = containerElem;}
+
+    get aspect(){return this.width / this.height;}
 }
 
