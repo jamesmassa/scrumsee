e0e54161f66dbbdc8567c36433d178d6f04d495b
James Massa <james.l.massa@gmail.com>
2020-10-12 09:59:37 -0400

    called stub api from python server and retruned payload to front end

--- analytics/README.md
+++ analytics/README.md
@@ -1,7 +1,9 @@
 TEST PROCEDURE
 0. Type . venv/bin/activate in the analytics directory
-1. Type flask run 
-2. Go to http://127.0.0.1:5000/api/get-json
-3. verify that json is returned
+1. Type export FLASK_ENV=development 
+2. Type export FLASK_DEBUG=1 
+3. Type flask run 
+4. Go to http://127.0.0.1:5000/api/get-json
+5. verify that json is returned
  
 
--- analytics/application.py
+++ analytics/application.py
@@ -1,21 +1,29 @@
 import os
+import requests
 
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+
 
 @app.route('/api/get-json')
 def hello():
     # TODO
-    #  1. Change hello world to a call to jira API
-    #  2. Add personal access token
-    #  3. Add analytics
-    #  4. Migrate more JS logic to the back-end
+    #  1. Change api stub call to jira API with personal access token
+    #  2. Add analytics
+    #  3. Migrate more JS logic to the back-end
 
-    response = jsonify(hello='world5')  # Returns HTTP Response with {"hello": "world"}
+    apiresponse = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    # apiresponse = requests.get("https://seescrum.atlassian.net/rest/agile/latest/board/1/issue")
+
+    response = jsonify(apiresponse.json())
     response.headers.add('Access-Control-Allow-Origin', '*')
+
     return response
 
 
